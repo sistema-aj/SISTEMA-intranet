ff1900dee4f4dc950ecd463c92b5381cc0c6513d
guemasAnthony <guemas12.anthony@gmail.com>
2014-02-28 13:39:17 +0100

    modification de la Classe Config

--- app/Config.php
+++ app/Config.php
@@ -2,24 +2,12 @@
 
 	class Config
 	{
-		private static $_nomSite		= "SISTEMA" ;
-		private static $_bdName			= "sistema";
-		private static $_bdUser			= "root";
-		private static $_bdHote			= "127.0.0.1";
-		private static $_bdPort			= "3306";
-		private static $_bdPassword		= "";
-
-
-
-		public function __get($var)
-		{
-
-			if(isset($this->$var))
-				return $this->$var;
-			else
-				throw new Exception('Variable de configuration "' + $var +'" non définie');
-		}
-
+		public static $_nomSite			= "SISTEMA" ;
+		public static $_bdName			= "sistema";
+		public static $_bdUser			= "root";
+		public static $_bdHote			= "127.0.0.1";
+		public static $_bdPort			= "3306";
+		public static $_bdPassword		= "";
 
 	}
 
