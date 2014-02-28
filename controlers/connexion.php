8d4a53033cb2c5d721c954390b1850d4fb7b8a9d
Maxdweb <deleuil.maxime@gmail.com>
2014-02-28 18:55:17 +0100

    ajout de la gestion de session

--- controlers/connexion.php
+++ controlers/connexion.php
@@ -6,8 +6,17 @@
 
 	if(isset($_REQUEST['login']) && isset($_REQUEST['pwd'])) {
 		if($_REQUEST['login'] != "" && $_REQUEST['pwd'] != "") {
-			// traitement de la connexion
-		} else { // affiche page connexion
+			$user = bdd::getLogin($_REQUEST['login'], $_REQUEST['pwd']);
+			if(!empty($user)) {
+				// on enregistre ses données en variable SESSION
+				$_SESSION['user_id'] = $user->user;
+				$_SESSION['user_type'] = $user->type;
+				// redirection vers l'accueil
+				header('home'); 
+			} else { // affiche message erreur : données incorrectes
+				$data->error = "Le login ou mot de passe est incorrect";
+			}
+		} else { // affiche message erreur : manque données
 			$data->error = "Veuillez saisir tous les champs";
 		}
 	}
