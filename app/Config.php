4b4be34e4d56b17a022eef94a73272791e5f6b21
guemasAnthony <guemas12.anthony@gmail.com>
2014-02-28 13:22:51 +0100

    ajout de la classe Config

---
+++ app/Config.php
@@ -0,0 +1,14 @@
+<?php
+
+	class Config
+	{
+		private static $_nomSite		= "SISTEMA" ;
+		private static $_bdName			= "sistema";
+		private static $_bdUser			= "root";
+		private static $_bdHote			= "127.0.0.1";
+		private static $_bdPort			= "3306";
+		private static $_bdPassword		= "";
+
+	}
+
+?>
