b99c2f47a27b0200edb8333e0c6c98700a397e43
Maxdweb <deleuil.maxime@gmail.com>
2014-03-01 16:59:13 +0100

    up: ajout des sous menus

--- views/aSubMenu.php
+++ views/aSubMenu.php
@@ -12,18 +12,17 @@ switch ($_REQUEST["page"]) {
 	case 'clients': ?>
 		<div id="adminSubMenu">
 			<ul>
-				<li><a href="clients">...</a></li>
-				<li><a href="clients">...</a></li>
-				<li><a href="clients">...</a></li>
+				<li><a href="clients/liste">Liste complète</a></li>
+				<li><a href="clients/ajout">Ajout d'un client</a></li>
 			</ul>
 		</div>
 	<?php break;
 	case 'projets': ?>
 		<div id="adminSubMenu">
 			<ul>
-				<li><a href="projets">...</a></li>
-				<li><a href="projets">...</a></li>
-				<li><a href="projets">...</a></li>
+				<li><a href="projets/liste">Liste complète</a></li>
+				<li><a href="projets/actifs-termines">Projets Actifs / Terminés</a></li>
+				<li><a href="projets/ajout">Ajout d'un projet</a></li>
 			</ul>
 		</div>
 	<?php break; 
