bcba2df501164afc86b963f2d0151ab24cd1b710
guemasAnthony <guemas12.anthony@gmail.com>
2014-03-03 09:06:53 +0100

    ajout de la doc

--- models/Adherents.php
+++ models/Adherents.php
@@ -1,13 +1,33 @@
 <?php
+
+	/**
+	 * 
+	 * @package Model
+	 */
+
 	class Adherents {
+
+		/**
+		 * retourne la liste des adherents 
+		 * @return array liste d'identifiants
+		 */
 		public static function getAdherents() {
 
 		}
 
-		public static function getDetailAdherent() {
+		/**
+		 * retourne un adherent à partir de son identifiant
+		 * @param  identifiant de l'adherent 
+		 * @return adherent  Objet adherent correspondant à l'identifiant 
+		 */
+		public static function getDetailAdherent($identifiant) {
 
 		}
 
+		/**
+		 * retourne la liste des adherents participant au projet passé en parametres
+		 * @return Array liste d'identifiants
+		 */
 		public static function getAdherentsParProjet() {
 
 		}
