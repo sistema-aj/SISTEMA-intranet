c414efac1825a8db8537bb4151fa3cff97904c7f
guemasAnthony <guemas12.anthony@gmail.com>
2014-03-04 17:43:40 +0100

    completion des methodes

--- models/Adherents.php
+++ models/Adherents.php
@@ -4,29 +4,91 @@
 	 * @package Models
 	 */
 
-	class Adherents {
+	class Adherents extends utilisateur 
+	{
+
+		private $prenom;
+		private $nom;
+
+		/**
+		 * constructeur d'Adherent 
+		 * @todo Verifier le "super"
+		 * 
+		 * @author  Guemas Anthony
+		 * @version 1
+		 */
+		public Adherent($id, $tel, $mail, $adresse, $actif, $prenom, $nom)
+		{
+			super($id, $tel, $mail, $adresse, $actif);
+
+			this.$prenom = $prenom;
+			this.$nom = $nom;
+		}
 
 		/**
 		 * retourne la liste des adherents 
 		 * 
-		 * @author 
+		 * @author  Guemas Anthony
 		 * @return  array liste d'identifiants
-		 * @version 0
+		 * @version 1
 		 */
-		public static function getAdherents() {
+		public static function getAdherents() 
+		{
+			try
+			{
+				$result = self::$_pdo->query("SELECT id
+											  FROM adherent");
 
+				$result->setFetchMode(PDO::FETCH_OBJ);
+				$result = $result->fetchAll();
+				return $result;
+			}
+			catch(Exception $e)
+			{
+				return $e;
+			}
 		}
 
 		/**
 		 * retourne un adherent à partir de son identifiant
-		 * 
-		 * @author 
+		 *  
+		 * @author  Guemas Anthony
 		 * @param   $idAdherent  identifiant de l'adherent 
 		 * @return  adherent     Objet adherent correspondant à l'identifiant 
-		 * @version 0
+		 * @version 1
 		 */
-		public static function getDetailAdherent($idAdherent) {
+		public static function getDetailAdherent($idAdherent) 
+		{
+			try
+			{
+				// recuperation des informations
+				$result = bdd::$_pdo->prepare(" SELECT id, telephone, mail, adresse, actif, nom, prenom 
+												FROM utilisateur JOIN adherent ON utilisateur.id = adherent.id 
+												AND utilisateur.id = :id");
+				$result->bindParam(":id", $idAdherent, PDO::PARAM_INT);
+				$result->setFetchMode(PDO::FETCH_OBJ);
+				$result = $result->fetchAll();
+				//return $result;
 
+				$id      = $result->id;
+				$tel     = $result->tel;
+				$mail    = $result->mail;
+				$adresse = $result->adresse;
+				$actif   = $result->actif;
+				$nom     = $result->nom;
+				$prenom  = $result->prenom;
+
+
+				// encapsulation
+				Adherent A = new Adherent($id, $tel, $mail, $adresse, $actif, $nom, $prenom);
+
+				return A;
+
+			}
+			catch(Exception $e)
+			{
+				return $e;
+			}
 		}
 
 		/**
@@ -37,8 +99,24 @@
 		 * @return  Array      liste d'identifiants
 		 * @version 0
 		 */
-		public static function getAdherentsParProjet($idProjet) {
-
+		public static function getAdherentsParProjet($idProjet)
+		{
+			try
+			{
+				// recuperation des informations
+				$result = bdd::$_pdo->prepare(" SELECT id
+												FROM utilisateur JOIN adherent ON utilisateur.id = adherent.id
+												JOIN projet ON utilisateur.id = projet.user 
+												AND projet.id = :id");
+				$result->bindParam(":id", $idProjet, PDO::PARAM_INT);
+				$result->setFetchMode(PDO::FETCH_OBJ);
+				$result = $result->fetchAll();
+				return $result;
+			}
+			catch(Exception $e)
+			{
+				return $e;
+			}
 		}
 	}
 ?>
