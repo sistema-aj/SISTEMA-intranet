1492ce71193aea9342635e6bd72d190033e291c2
Maxdweb <deleuil.maxime@gmail.com>
2014-03-04 13:59:33 +0100

    add: nouvelle constante URL_ROOT

--- views/aMenu.php
+++ views/aMenu.php
@@ -1,7 +1,8 @@
 <div id="adminMenu">
 	<ul>
-		<li><a href="adherents">Gestion adhérents</a></li>
-		<li><a href="clients">Gestion clients</a></li>
-		<li><a href="projets">Gestion projets</a></li>
+		<li><a href="<?php echo URL_ROOT; ?>home">Accueil</a></li>
+		<li><a href="<?php echo URL_ROOT; ?>adherents">Gestion adhérents</a></li>
+		<li><a href="<?php echo URL_ROOT; ?>clients">Gestion clients</a></li>
+		<li><a href="<?php echo URL_ROOT; ?>projets">Gestion projets</a></li>
 	</ul>
 </div>
