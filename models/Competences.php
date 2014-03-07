0c222c1cb5628f4f618fa605f5b0b5dc05b91fee
guemasAnthony <guemas12.anthony@gmail.com>
2014-03-07 10:07:35 +0100

    completion des methodes

--- models/Competences.php
+++ models/Competences.php
@@ -3,18 +3,36 @@
 	/**
 	 * @package Models
 	 */
-	class Competences {
-
+	class Competences 
+	{
 		/**
 		 * ajoute une competence à l'adherent
 		 * 
-		 * @author
+		 * @author  Guemas Anthony
 		 * @param   String $idAdherent 
 		 * @param   int    $idCompetence 
-		 * @version 0
+		 * @version 1
 		 */
-		public function ajoutCompetence($idAdherent, $idCompetence){
+		public function ajoutCompetence($idAdherent, $idCompetence)
+		{
+		 	try
+            {
+                // recuperation des informations
+                $insert = bdd::$_pdo->prepare(" INSERT INTO detenir (user, competence)
+                                                VALUES (:idA , :idC)
+                                              ");
 
+                $insert->execute( 
+                                    array(
+                                            'idA'       => $idAdherent   ,
+                                            'idC' 		=> $idCompetence 
+                                        )
+                                );
+            }
+            catch(Exception $e)
+            {
+                return $e;
+            }
 		}
 
 		/**
@@ -25,8 +43,20 @@
 		 * @param  int    $idCompetence 
 		 * @version 0
 		 */
-		public function supppressionCompetence($idAdherent, $idCompetence){
+		public function supppressionCompetence($idAdherent, $idCompetence)
+		{
+			$delete = bdd::$_pdo->prepare(" DELETE FROM detenir 
+                                            WHERE 	    user 	    = :idA
+                                            AND    		compentence = :idC
+                                         ");
 
+            $delete->execute( 
+                                    array(
+                                            'idA'       => $idAdherent   ,
+                                            'idC' 		=> $idCompetence 
+                                        )
+                                );
+            }	
 		}
 
 
@@ -37,8 +67,25 @@
 		 * @param   String $nomCompetence 
 		 * @version 0
 		 */
-		public function listerNouvelleCompetence($nomCompetence){
+		public function listerNouvelleCompetence($nomCompetence)
+		{
+			try
+            {
+                // recuperation des informations
+                $insert = bdd::$_pdo->prepare(" INSERT INTO competences( nom )
+                                                VALUES (:nom)
+                                              ");
 
+                $insert->execute( 
+                                    array(
+                                            'nom'       => $nomCompetence   ,
+                                        )
+                                );
+            }
+            catch(Exception $e)
+            {
+                return $e;
+            }
 		}
 
 		/**
@@ -49,9 +96,30 @@
 		 * @return  array 	  Liste des identifiants des adherents  
 		 * @version 0
 		 */
-		public function getEtudiantParCompetence($idCompetence){
-	
+		public function getAdherentParCompetence($idCompetence)
+		{
+			 try
+            {
+                // recuperation des informations
+                $result = bdd::$_pdo->prepare(" SELECT   telephone, mail, adresse, codePostal, ville, actif, nom, prenom, promo
+                                                FROM     utilisateur JOIN adherent   ON utilisateur.id = adherent.id
+                                                                     JOIN detenir    ON adherent.id    = detenir.user 
+                                                WHERE    detenir.competence = :idC    
+                                             ");
+
+                $result->bindParam(":idC", $idCompetence, PDO::PARAM_INT);
+                $result = $result->fetchAll();
+
+                return $result;
+            }
+            catch(Exception $e)
+            {
+                return $e;
+            }
+        }
+
 		}
+
 	}
 
 
