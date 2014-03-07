428a05db0e98dc0d033b583bb3f99002e57b9a97
Maxdweb <deleuil.maxime@gmail.com>
2014-03-07 14:48:29 +0100

    new: views for clients

---
+++ views/aCliAjout.php
@@ -0,0 +1,17 @@
+<div id="aCliAjout">
+	<form name="addCli" action="" method="post">
+		<label>Raison Sociale :</label><br/>
+		<input type="text" name="raisonSociale"><br/>
+		<label>Telephone :</label>
+		<label>Mail :</label><br/>
+		<input type="text" name="telephone">
+		<input type="text" name="mail"><br/>
+		<label>Adresse :</label>
+		<label>Code Postal :</label><br/>
+		<input type="text" name="adresse">
+		<input type="text" name="codePostal"><br/>
+		<label>Ville :</label><br/>
+		<input type="text" name="ville"><br/>
+		<input type ="submit" value="Générer le Client"/>
+	</form>
+</div>
---
+++ views/aCliListe.php
@@ -0,0 +1,20 @@
+<table id="aCliListe" style="width:100%">
+	<tr>
+		<th>RaisonSociale</th>
+		<th>Adresse</th>
+		<th>Code Postal</th>
+		<th>Ville</th>
+		<th>Téléphone</th>
+		<th>Mail</th>
+	</tr>
+	<?php foreach ($data->clients as $client) { ?>
+	<tr>
+		<td><?php echo $client->raisonSociale; ?></td>
+		<td><?php echo $client->adresse; ?></td>
+		<td><?php echo $client->codePostal; ?></td>
+		<td><?php echo $client->ville; ?></td>
+		<td><?php echo $client->telephone; ?></td>
+		<td><?php echo $client->mail; ?></td>
+	</tr>
+	<?php } ?>
+</table>
