4d3866d2f6ee663365d620b4e2e9900253b118df
Maxdweb <deleuil.maxime@gmail.com>
2014-03-13 11:59:55 +0100

    correction des bugs + ajout fonction getCompetencesParAdh()

--- models/Competences.php
+++ models/Competences.php
@@ -13,25 +13,22 @@
 		 * @param   int    $idCompetence 
 		 * @version 1
 		 */
-		public function ajoutCompetence($idAdherent, $idCompetence)
+		public static function ajoutCompetence($idAdherent, $idCompetence)
 		{
 		 	try
             {
-                // recuperation des informations
-                $insert = bdd::$_pdo->prepare(" INSERT INTO detenir (user, competence)
-                                                VALUES (:idA , :idC)
-                                              ");
-
-                $insert->execute( 
-                                    array(
-                                            'idA'       => $idAdherent   ,
-                                            'idC' 		=> $idCompetence 
-                                        )
-                                );
+                bdd::$_pdo->beginTransaction();
+                $insert = bdd::$_pdo->prepare("INSERT INTO detenir (user, competence)
+                                                VALUES (:idA , :idC)");
+                $insert->bindParam(':idA',$idAdherent,PDO::PARAM_INT);
+                $insert->bindParam(':idC',$idCompetence,PDO::PARAM_INT);
+                $insert->execute();
+                bdd::$_pdo->commit();
             }
             catch(Exception $e)
             {
-                return $e;
+                bdd::$_pdo->rollBack();
+                throw new Exception($e->getMessage());
             }
 		}
 
@@ -43,21 +40,22 @@
 		 * @param  int    $idCompetence 
 		 * @version 0
 		 */
-		public function supppressionCompetence($idAdherent, $idCompetence)
+		public static function supppressionCompetence($idAdherent, $idCompetence)
 		{
-			$delete = bdd::$_pdo->prepare(" DELETE FROM detenir 
-                                            WHERE 	    user 	    = :idA
-                                            AND    		compentence = :idC
-                                         ");
-
-            $delete->execute( 
-                                    array(
-                                            'idA'       => $idAdherent   ,
-                                            'idC' 		=> $idCompetence 
-                                        )
-                                );
-            }	
-		}
+            try {
+                bdd::$_pdo->beginTransaction();
+        		$delete = bdd::$_pdo->prepare("DELETE FROM detenir 
+                                                WHERE user = :idA
+                                                AND compentence = :idC");
+                $delete->bindParam(":idA",$idAdherent,PDO::PARAM_INT);
+                $delete->bindParam(":idC",$idCompetence,PDO::PARAM_INT);
+                $delete->execute();
+                bdd::$_pdo->commit();
+            } catch (Exception $e) {
+                bdd::$_pdo->rollBack();
+                throw new Exception($e->getMessage());
+            }
+        }	
 
 
 		/**
@@ -67,64 +65,72 @@
 		 * @param   String $nomCompetence 
 		 * @version 0
 		 */
-		public function listerNouvelleCompetence($nomCompetence)
+		public static function listerNouvelleCompetence($nomCompetence)
 		{
 			try
             {
-                // recuperation des informations
-                $insert = bdd::$_pdo->prepare(" INSERT INTO competences( nom )
-                                                VALUES (:nom)
-                                              ");
-
-                $insert->execute( 
-                                    array(
-                                            'nom'       => $nomCompetence   ,
-                                        )
-                                );
+                bdd::$_pdo->beginTransaction();
+                $insert = bdd::$_pdo->prepare("INSERT INTO competences(nom)
+                                                VALUES (:nom)");
+                $insert->bindParam(":nom",$nomCompetence,PDO::PARAM_STR);
+                $insert->execute();
+                bdd::$_pdo->commit();
             }
             catch(Exception $e)
             {
-                return $e;
+                bdd::$_pdo->rollBack();
+                throw new Exception($e->getMessage());
             }
 		}
 
 		/**
 		 * récupère la liste des etudiants possedant la competence demandée 
 		 * 
-		 * @author 
+		 * @author  Guemas Anthony
 		 * @param   int 	  $idCompetence 
 		 * @return  array 	  Liste des identifiants des adherents  
 		 * @version 0
 		 */
-		public function getAdherentParCompetence($idCompetence)
+		public static function getAdherentParCompetence($idCompetence)
 		{
 			 try
             {
-                // recuperation des informations
-                $result = bdd::$_pdo->prepare(" SELECT   telephone, mail, adresse, codePostal, ville, actif, nom, prenom, promo
-                                                FROM     utilisateur JOIN adherent   ON utilisateur.id = adherent.id
-                                                                     JOIN detenir    ON adherent.id    = detenir.user 
-                                                WHERE    detenir.competence = :idC    
-                                             ");
-
+                $result = bdd::$_pdo->prepare(" SELECT telephone, mail, adresse, codePostal, ville, actif, nom, prenom, promo
+                                                FROM utilisateur JOIN adherent ON utilisateur.id = adherent.id
+                                                JOIN detenir ON adherent.id = detenir.user 
+                                                WHERE detenir.competence = :idC");
                 $result->bindParam(":idC", $idCompetence, PDO::PARAM_INT);
+                $result->execute();
                 $result = $result->fetchAll();
-
-                return $result;
             }
             catch(Exception $e)
             {
-                return $e;
+                throw new Exception($e->getMessage());
             }
         }
 
-		}
-
-	}
-
-
-
-
+        /**
+         * récupère la liste des compétences pour un étudiant 
+         * 
+         * @author  Deleuil Maxime
+         * @param   int       $id 
+         * @return  array     Liste des compétences de l'adhérent  
+         * @version 1.0.0
+         */
+        public static function getCompetencesParAdh($id) {
+            try {
+                $result = bdd::$_pdo->prepare("SELECT nom FROM detenir JOIN competences ON id = competence 
+                                            WHERE user = :id");
+                $result->bindParam(":id", $id, PDO::PARAM_INT);
+                $result->execute();
+                $result->setFetchMode(PDO::FETCH_OBJ);
+                $result = $result->fetchAll();
+                return $result;
+            } catch (Exception $e) {
+                return $e;
+            }
+        }
+    }
 ?>
 
 
